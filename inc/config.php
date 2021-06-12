4d35a2f8cf754f57fa2e43d7b7fccc791a63d3e1
Your Name <you@example.com>
2021-06-11 22:34:23 -0800

    changes on config file and connection mysql work banch

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,11 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "inventory");
+//define("DB_HOST", "localhost");
+//define("DB_USER", "root");
+//define("DB_PASS", "");
+//define("DB_NAME", "inventory");
+
+define("DB_HOST", "remotemysql.com");
+define("DB_USER", "oYfQCqpDIH");
+define("DB_PASS", "EMITLlakIq");
+define("DB_NAME", "oYfQCqpDIH");
+
